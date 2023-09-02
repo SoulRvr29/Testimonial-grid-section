a124c6a49bb2d49932ae64369f9d0e83abd6a84a
Paweł Chudecki <pawelc29@interia.pl>
2023-09-02 18:15:57 +0200

    grid update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import testimonials from "./testimonials.json";
 
 function App() {
   return (
-    <main className="max-w-2xl">
+    <main className="max-w-[1140px] pt-14 grid grid-cols-4 max-lg:grid-cols-none max-lg:grid-rows-none grid-rows-1 max-lg:max-w-2xl mx-4">
       {testimonials.map((item) => (
         <Article data={item} key={item.id} />
       ))}
--- src/Article.jsx
+++ src/Article.jsx
@@ -3,14 +3,28 @@ const Article = ({ data }) => {
     <article
       className={
         data.id == 1
-          ? "bg-Moderate-violet mx-8 rounded-lg p-10 text-white my-8 shadow-xl"
+          ? "bg-Moderate-violet  rounded-lg px-10 py-8 text-white  shadow-xl leading-[1.4] relative col-span-2 m-4 max-lg:col-span-1"
           : data.id == 2
-          ? "bg-Very-dark-grayish-blue mx-8 rounded-lg p-10 text-white my-8 shadow-xl"
-          : data.id == 3 || data.id == 5
-          ? "bg-white mx-8 rounded-lg p-10 text-Very-dark-blackish-blue my-8 shadow-xl"
-          : "bg-Very-dark-blackish-blue mx-8 rounded-lg p-10 text-white my-8 shadow-xl"
+          ? "bg-Very-dark-grayish-blue rounded-lg px-10 py-8 text-white  shadow-xl leading-[1.4] m-4"
+          : data.id == 3
+          ? "bg-white  rounded-lg px-10 py-8 text-Very-dark-blackish-blue shadow-xl leading-[1.4] m-4 row-span-2 order-1 max-lg:col-span-1"
+          : data.id == 4
+          ? "bg-Very-dark-blackish-blue  rounded-lg px-10 py-8 text-white  shadow-xl leading-[1.4] m-4 col-span-2 order-3 max-lg:col-span-1 "
+          : "bg-white  rounded-lg px-10 py-8 text-Very-dark-blackish-blue shadow-xl leading-[1.4] m-4 row-span-2 max-lg:col-span-1 max-lg:order-5"
       }
     >
+      <svg
+        className={data.id == 1 ? "absolute right-8 top-0" : "hidden"}
+        width="104"
+        height="102"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M104 102V59.727H84.114c0-5.871.689-11.182 2.068-15.933 1.379-4.75 3.42-9.287 6.125-13.61C95.01 25.86 98.909 22.257 104 19.375V0c-9.758 4.27-17.712 9.874-23.864 16.813-6.151 6.939-10.712 14.545-13.681 22.818C63.485 47.904 62 59.941 62 75.74V102h42zm-62 0V59.727H22.114c0-5.871.689-11.182 2.068-15.933 1.379-4.75 3.42-9.287 6.125-13.61C33.01 25.86 36.909 22.257 42 19.375V0c-9.652 4.27-17.58 9.874-23.784 16.813C12.01 23.752 7.424 31.358 4.455 39.631 1.485 47.904 0 59.941 0 75.74V102h42z"
+          fill="#A775F1"
+          fill-rule="nonzero"
+        />
+      </svg>
       <div className="flex">
         <img
           className="rounded-full border-2 border-white border-opacity-30 w-10 h-10 mr-4"
@@ -22,7 +36,7 @@ const Article = ({ data }) => {
           <p className="text-sm opacity-50">Verified Graduate</p>
         </div>
       </div>
-      <h3 className="font-bold text-2xl my-4">{data.review}</h3>
+      <h3 className="font-bold text-2xl my-4 z-10 relative">{data.review}</h3>
       <p className="opacity-70">{data.quotation}</p>
     </article>
   );
